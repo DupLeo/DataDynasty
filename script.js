f9516a9108b811cd30dd30784a73f356a9d19d07
leo <leodupraz.r@gmail.com>
2022-08-18 15:15:19 +0200

    Chart Bar City

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const barCanvas = document.getElementById('barCanvas');
 
 const barChart = new Chart(barCanvas,{
-    type:"bar",
+    type:"bar", // style du graphique (ici bar)
     data:{
-        labels:["Paris", "Tokyo","Berlin","Berne"],
+        labels:["Paris", "Tokyo","Berlin","Berne"], // Nom des barres
         datasets:[{
-            data:[240,120,140,130]
+            label:["dépense"],
+            data:[240,120,140,130], // Valeurs des barres
+            backgroundColor:["crimson","black","lightgreen","violet"] // Couleurs des barres
         }]
+    },
+    options:{
+        scales:{
+        y:{
+            suggestedMax:500, // taille maximal en ordonnée
+            ticks:{
+                font:{
+                    size:20, // taille police en ordonnée
+                }
+            },
+        },
+        x:{
+            ticks:{
+                font:{
+                    size:22, // taille police en abcisse
+                }
+            },
+        }
+    }
     }
 });
 
